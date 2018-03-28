e4135f32519513d38d5c9014eec924c7af583fd8
RemindYZ <yzhao14thu@163.com>
2018-03-28 18:19:00 +0800

    add C04 note

---
+++ code/C04_决策树/DecisionTree.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# python 3.6
+# author: Remind
+# C04-DecisionTree-4.3
+
+class Node:
+    def __init__(self, attr, number, compare=0):
+        self.attr = attr
+        self.number = number
+        self.compare = 0 # 0:
